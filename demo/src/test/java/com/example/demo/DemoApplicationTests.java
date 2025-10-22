2c30f13e77d2f2e1fa1a3ff25acae31e945ed1d4
Purple003 <wwepwawet@gmail.com>
2025-10-22 13:28:55 +0100

    first commit

---
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.swing.*;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootTest
+class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+}
